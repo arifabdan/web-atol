5d09ffd4fcbf79b180306938327ffff6f1c302ef
arifabdan <arifabdan73@gmail.com>
2021-05-12 21:48:42 +0700

    3rd Commit

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <title>Selamat Datang!</title>
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
   <a class="navbar-brand">
-  <img src="../web atol/img/bezier2.svg" width="35" height="35" alt="">
+  <img src="../web atol/img/code.png" width="35" height="35" alt="">
   Code73
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
