e9d593126e3fcef396ba5c21069d500634b949d9
arifabdan <arifabdan73@gmail.com>
2021-05-12 21:28:45 +0700

    3rd Commit

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">  
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -22,13 +24,13 @@
         <a class="nav-link" href="#">Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="tugas.php">Tugas</a>
+        <a class="nav-link" href="tugas.php">coursework</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Random</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="biodata.php">Biodata</a>
+        <a class="nav-link" href="biodata.php">profile</a>
       </li>
     </ul>
   </div>
@@ -37,39 +39,55 @@
 <div class="container-fluid">
  <div class="banner-text">
   <div class="text-area">
-     <span>s</span>
+     <span>w</span>
      <span>e</span>
      <span>l</span>
-     <span>a</span>
+     <span>c</span>
+     <span>o</span>
      <span>m</span>
-     <span>a</span>
-     <span>t</span>&nbsp;
-     <span>d</span>
-     <span>a</span>
-     <span>t</span>
-     <span>a</span>
-     <span>n</span>
-     <span>g</span>
+     <span>e</span>
   </div>
   <hr>
-   <p>Selamat datang selamat menikmati konten dari website ini dan jangan lupa selalu semangat dalam menjalani hidup</p>
+   <p>Welcome and enjoy the content with cup of tea</p>
   </div>
-  <div class="contact-info mr-auto">
-        <i class="icofont-envelope"></i> <a href="mailto:contact@example.com">contact@example.com</a>
-        <i class="icofont-phone"></i> +1 5589 55488 55
-        <i class="icofont-google-map"></i> A108 Adam Street, NY
-      </div>
-      <div class="social-links">
-        <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
-        <a href="#" class="facebook"><i class="icofont-facebook"></i></a>
-        <a href="#" class="instagram"><i class="icofont-instagram"></i></a>
-        <a href="#" class="skype"><i class="icofont-skype"></i></a>
-        <a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
-      </div>
  </div>
 </header> 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<footer>
+  <div class="footer-top">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12 segment-one" data-aos="fade-right" data-aos-duration="1000">
+          <h2>profile</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum fuga sequi earum delectus error odit quas pariatur harum aperiam, ullam vel nostrum quisquam, doloribus sint a neque nulla nihil natus.</p>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12 segment-two" data-aos="zoom-in-up" data-aos-duration="1000">
+          <h2>Links</h2>
+          <ul>
+            <li><a href="https://github.com/arifabdan">Github</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12 segment-three" data-aos="zoom-in-up" data-aos-duration="1000">
+          <h2>Follow Us</h2>
+          <p>Please Follow us on our social media profile in order to keep updated.</p>
+          <a href="https://www.facebook.com/arif.abdan.5/"><i class="fa fa-facebook"></i></a>
+          <a href="https://www.instagram.com/arifabdnsykr/?hl=en"><i class="fa fa-instagram"></i></a>
+          <a href="https://github.com/arifabdan"><i class="fa fa-github"></i></a>  
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12 segment-four" data-aos="fade-left" data-aos-duration="1000">
+          <h2>Gaming Stuff</h2>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, molestiae. Illum, numquam quo quos illo animi dolorem, hic cum dolorum beatae sequi, quae maxime eligendi ipsam odio molestias iste accusamus.</p>
+          <a href="https://steamcommunity.com/id/Jbxseven/"><i class="fa fa-steam-square"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<script>
+  AOS.init();
+</script>
   </body>
 </html>
